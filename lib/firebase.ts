3d4e445838850c2f11f22da28277cf52a14d47a4
Measly543 <148374770+EDUARDOVAZQUE@users.noreply.github.com>
2026-02-12 21:32:35 -0600

    only test

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -26,13 +26,13 @@ import {
 } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "mock_key",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "mock_domain",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "mock_project_id",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "mock_bucket",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "mock_sender_id",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "mock_app_id",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "mock_measurement_id"
+  apiKey: "AIzaSyDh99IVYNqsie8ex_ko9CQUJlzl2vnyDfQ",
+  authDomain: "racing-cup-dbd5a.firebaseapp.com",
+  projectId: "racing-cup-dbd5a",
+  storageBucket: "racing-cup-dbd5a.firebasestorage.app",
+  messagingSenderId: "273529090223",
+  appId: "1:273529090223:web:d8dfef90a763641a9a245a",
+  measurementId: "G-PWF78H8925"
 }
 
 // Initialize Firebase
