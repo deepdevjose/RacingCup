ed90b805e29bb2b56de11c18658049beeebb6edb
Measly543 <148374770+EDUARDOVAZQUE@users.noreply.github.com>
2026-03-11 22:11:12 -0600

    Change link to eventos page in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -133,7 +133,7 @@ export default function DashboardPage() {
                         </div>
                     </Link>
 
-                    <Link href="/dashboard/equipos" className="quick-action-card">
+                    <Link href="/dashboard/eventos/dNa2TptMBEaGHRRsGx5V" className="quick-action-card">
                         <div className="quick-action-icon">
                             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                 <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
